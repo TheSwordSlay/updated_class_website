a14d67478a0a26229fe74608f09fc9facaf7c329
TheSwordSlay <fortugtss@gmail.com>
2022-12-10 07:04:39 +0700

    added javascript for automatic open/close form

---
+++ absen/insert_imk.php
@@ -0,0 +1,44 @@
+<?php
+$nim = $_POST['nim'];
+$nama = $_POST['nama'];
+$kehadiran = $_POST['kehadiran'];
+
+
+if (!empty($nim) || !empty($nama) || !empty($kehadiran)) {
+    $host = "localhost";
+    $dbUsername = "root";
+    $dbPassword = "";
+    $dbname = "kelasl3";
+
+    $conn = new mysqli($host, $dbUsername, $dbPassword, $dbname);
+
+    if (mysqli_connect_error()) {
+        header("Location: absen_gagal.html");
+        die();
+    } else {
+        $SELECT = "select nim from absen_imk where nim = ? Limit 1";
+        $INSERT = "insert into absen_imk (nim, nama, kehadiran) values (?, ?, ?)";
+
+        $stmt = $conn->prepare($SELECT);
+        $stmt->bind_param("s", $nim);
+        $stmt->execute();
+        $stmt->bind_result($nim);
+        $stmt->store_result();
+
+            $stmt->close();
+
+            $stmt = $conn->prepare($INSERT);
+            $stmt->bind_param("sss", $nim, $nama, $kehadiran);
+            $stmt->execute();
+            // echo "Data absensi tercatat";
+
+        $stmt->close();
+        $conn->close();
+        header("Location: absen_sukses.html");
+        die();
+    }
+} else {
+    header("Location: absen_gagal.html");
+    die();
+}
+?>
---
+++ absen/si.js
@@ -0,0 +1,13 @@
+const weekday = ["Minggu","Senin","Selasa","Rabu","Kamis","Jum'at","Sabtu"];
+let buka = document.getElementById("buka");
+let tutup = document.getElementById("tutup");
+
+
+const d = new Date();
+let day = weekday[d.getDay()];
+
+if (day == "Sabtu") {
+    tutup.innerHTML = "";
+} else {
+    buka.innerHTML = "";
+}
